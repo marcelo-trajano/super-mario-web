172c3151f7b07946e14fb6c41396d9d482b9a48e
Marcelo <marcelo.trajano@outlook.com>
2020-11-08 01:48:02 -0300

    ٩(◕‿◕｡)۶

---
+++ src/App.js
@@ -0,0 +1,91 @@
+import React, { useState, useEffect } from "react";
+import "./App.css";
+const ALLOWED_KEYS = ["ArrowUp", "ArrowDown", "ArrowLeft", "ArrowRight"];
+const MAX_Y = 570;
+const MIN_Y = 30;
+const MAX_X = 1230;
+const MIN_X = 30;
+
+export default () => {
+  const [pressed, setPressed] = useState([]);
+  const [positionX, setPositionX] = useState(0);
+  const [positionY, setPositionY] = useState(0);
+  const [flipOver, setFlipOver] = useState(false);
+
+  useEffect(() => {
+    document.addEventListener("keydown", handleKeyDown);
+    localStorage.setItem("positionX", positionX);
+    localStorage.setItem("positionY", positionY);
+  }, []);
+
+  const handleKeyDown = React.useCallback(
+    (event) => {
+      const { key } = event;
+      if (ALLOWED_KEYS.includes(key) && !pressed.includes(key)) {
+        let pos = 0;
+        switch (key) {
+          case "ArrowRight":
+            setFlipOver(false);
+            pos = parseInt(localStorage.getItem("positionX"));
+            if (pos < MAX_X) {
+              pos += 30;
+              localStorage.setItem("positionX", pos);
+              setPositionX(pos);
+              console.log("x: " + pos);
+            }
+            break;
+
+          case "ArrowLeft":
+            setFlipOver(true);
+            pos = parseInt(localStorage.getItem("positionX"));
+            if (pos >= MIN_X) {
+              pos -= 30;
+              localStorage.setItem("positionX", pos);
+              setPositionX(pos);
+            }
+            console.log("x: " + pos);
+            break;
+
+          case "ArrowUp":
+            pos = parseInt(localStorage.getItem("positionY"));
+            if (pos < MAX_Y) {
+              pos += 30;
+              localStorage.setItem("positionY", pos);
+              setPositionY(pos);
+              console.log("Y: " + pos);
+            }
+            break;
+
+          case "ArrowDown":
+            pos = parseInt(localStorage.getItem("positionY"));
+            if (pos >= MIN_Y) {
+              pos -= 30;
+              localStorage.setItem("positionY", pos);
+              setPositionY(pos);
+              console.log("Y: " + pos);
+            }
+            break;
+        }
+
+        setPressed([...pressed, key]);
+      }
+    },
+    [pressed]
+  );
+
+  return (
+    <div className="App">
+      <div className="mario-world">
+        <div className="move-field">
+          <div
+            className={`mario ${flipOver ? `flip-over` : ``}`}
+            style={{
+              marginLeft: `${positionX}px`,
+              marginBottom: `${positionY}px`,
+            }}
+          ></div>
+        </div>
+      </div>
+    </div>
+  );
+};
